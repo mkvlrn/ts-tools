86edb54b95025a8b9e63aa3d6d9459e7f95957b0
mkvlrn <mkvlrn@gmail.com>
2026-08-05 11:02:23 -0300

    docs(app-error): update JSDoc for httpStatus export

--- packages/app-error/src/main.ts
+++ packages/app-error/src/main.ts
@@ -1,5 +1,5 @@
 /** biome-ignore-all lint/performance/noBarrelFile: package exports */
 
-export { AppError } from "./app-error";
+export { AppError, type AppErrorFactory } from "./app-error";
 export type { StatusCode, StatusName, StatusPhrase } from "./consts";
-export { httpStatus } from "./status";
+export { type HttpStatus, httpStatus } from "./status";
